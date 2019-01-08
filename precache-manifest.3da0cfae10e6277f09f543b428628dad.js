e9fa8ba4a748f2e6f886845002a08344b2acbcb2
maximys963 <maxim.p.jsdev@gmail.com>
2019-01-08 21:10:09 +0200

    Updates

---
+++ precache-manifest.3da0cfae10e6277f09f543b428628dad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "10ba268311376618ccbe1a9aa67a5b6f",
+    "url": "/browser/static/media/searching-magnifying-glass.10ba2683.svg"
+  },
+  {
+    "revision": "e01686f861c84d9f597f2aafebc982f0",
+    "url": "/browser/static/media/camera.e01686f8.svg"
+  },
+  {
+    "revision": "78535ff0b41d47386757fdc9c969428c",
+    "url": "/browser/static/media/boobs-clipart-icon-6.78535ff0.png"
+  },
+  {
+    "revision": "e7e5eb35268b57e18db7",
+    "url": "/browser/static/js/runtime~main.e7e5eb35.js"
+  },
+  {
+    "revision": "c02773b3ba4a6b762fd5",
+    "url": "/browser/static/js/main.c02773b3.chunk.js"
+  },
+  {
+    "revision": "4abbf8387f206c7c4ce1",
+    "url": "/browser/static/js/1.4abbf838.chunk.js"
+  },
+  {
+    "revision": "c02773b3ba4a6b762fd5",
+    "url": "/browser/static/css/main.73673728.chunk.css"
+  },
+  {
+    "revision": "209c552943d70c976bcf4afab0b8d69f",
+    "url": "/browser/index.html"
+  }
+];
