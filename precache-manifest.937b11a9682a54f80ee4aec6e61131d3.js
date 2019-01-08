b1f75fde7480618615cdd729fdc94e1bd8c65ddd
maximys963 <maxim.p.jsdev@gmail.com>
2019-01-08 21:19:54 +0200

    Updates

---
+++ precache-manifest.937b11a9682a54f80ee4aec6e61131d3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "10ba268311376618ccbe1a9aa67a5b6f",
+    "url": "/Hight-Art/static/media/searching-magnifying-glass.10ba2683.svg"
+  },
+  {
+    "revision": "e01686f861c84d9f597f2aafebc982f0",
+    "url": "/Hight-Art/static/media/camera.e01686f8.svg"
+  },
+  {
+    "revision": "78535ff0b41d47386757fdc9c969428c",
+    "url": "/Hight-Art/static/media/boobs-clipart-icon-6.78535ff0.png"
+  },
+  {
+    "revision": "96e0a834008363836e3a",
+    "url": "/Hight-Art/static/js/runtime~main.96e0a834.js"
+  },
+  {
+    "revision": "cbd0acd004daa0cad36f",
+    "url": "/Hight-Art/static/js/main.cbd0acd0.chunk.js"
+  },
+  {
+    "revision": "42912ffc41913a82b178",
+    "url": "/Hight-Art/static/js/1.42912ffc.chunk.js"
+  },
+  {
+    "revision": "cbd0acd004daa0cad36f",
+    "url": "/Hight-Art/static/css/main.73673728.chunk.css"
+  },
+  {
+    "revision": "f112defd2fa7ccf0633252f794295b8b",
+    "url": "/Hight-Art/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/browser/precache-manifest.e1ac64ed2cfc65dcd24224b772ab3bb5.js"
+  "/Hight-Art/precache-manifest.937b11a9682a54f80ee4aec6e61131d3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/browser/index.html", {
+workbox.routing.registerNavigationRoute("/Hight-Art/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
